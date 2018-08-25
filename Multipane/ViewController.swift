4f7d7517a2e67db96723f6402ddebdf816a322fe
Steve Lipton <steve@makeapppie.com>
2018-08-25 12:01:35 -0500

    Initial commit Begin State 04_05 iPad Rotation

--- Multipane/ViewController.swift
+++ Multipane/ViewController.swift
@@ -1,20 +1,56 @@
 //
 //  ViewController.swift
-//  Multipane
+//  Multipane rotation - Demonstrates landscape layout on iPad
 //
-//  Created by Steven Lipton on 8/24/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
 //
-
+//  A exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
+//
 import UIKit
 
 class ViewController: UIViewController {
+//Outlets
+    @IBOutlet weak var pizzaImageView: UIImageView!
+    @IBOutlet weak var rootStackView: UIStackView!
 
+// Computed properties for convenience
+    var isRegularHeight:Bool{
+        return view.traitCollection.verticalSizeClass == .regular
+    }
+    var isCompactWidth:Bool{
+        return view.traitCollection.horizontalSizeClass == .compact
+    }
+    
+// The layout methods
+    func landscapeLayout(){
+        rootStackView.axis = .horizontal
+        pizzaImageView.image = UIImage(named:"Pizza Landscape")
+    }
+    
+    func portraitLayout(){
+        rootStackView.axis = .vertical
+        pizzaImageView.image = UIImage(named: "Pizza Portrait")
+    }
+
+    override func viewWillLayoutSubviews() {
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
-
